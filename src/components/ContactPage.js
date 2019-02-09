82bfcb343d0ea258383714797b31317f50a39e9c
Jem Alvarez <jemalvarezcontact@gmail.com>
2019-02-09 11:16:50 -0500

    Contact page style finished

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import { MdLocationOn } from 'react-icons/md'
+import { FiSend } from 'react-icons/fi'
 
 const ContactPage = () => (
-    <div>
-        <h1>Heading</h1>
-        <h3>Subheading</h3>
-        <form>
-            <input />
-            <input />
-            <textarea></textarea>
-            <div>
-                <button>Location</button>
-                <button>Submit</button>
+    <div className="container contact">
+        <h1 className="contact__heading">Heading</h1>
+        <h3 className="contact__subheading">Subheading</h3>
+        <form name="contact" className="contact__form" netlify>
+            <input autoComplete="new-password" type="text" className="contact__input" placeholder="John Doe" required />
+            <input autoComplete="new-password" type="email" className="contact__input" placeholder="johndoe@email.com" required />
+            <textarea autoComplete="new-password" maxLength="120" rows="4" placeholder="How can we work together?" required></textarea>
+            <div className="contact__buttons">
+                <button
+                    className="btn btn--location"
+                    onClick={(e) => {
+                        e.preventDefault()
+                    }}
+                >
+                    <MdLocationOn />
+                </button>
+                <button
+                    className="btn btn--submit"
+                // onClick={(e) => {
+                //     e.preventDefault()
+                // }}
+                >
+                    <FiSend />
+                </button>
             </div>
         </form>
     </div>
