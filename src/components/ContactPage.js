31d8f037fcd2c7d38e1da3cc61619293b486f68f
Jem Alvarez <jemalvarezcontact@gmail.com>
2019-02-09 11:23:43 -0500

    Fix #2

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,7 +6,7 @@ const ContactPage = () => (
     <div className="container contact">
         <h1 className="contact__heading">Heading</h1>
         <h3 className="contact__subheading">Subheading</h3>
-        <form name="contact" className="contact__form" data-netlify="true">
+        <form action="POST" name="contact" className="contact__form" data-netlify="true">
             <input autoComplete="new-password" type="text" className="contact__input" placeholder="John Doe" required />
             <input autoComplete="new-password" type="email" className="contact__input" placeholder="johndoe@email.com" required />
             <textarea autoComplete="new-password" maxLength="120" rows="4" placeholder="How can we work together?" required></textarea>
