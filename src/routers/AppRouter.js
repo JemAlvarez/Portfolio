74b7e16c5b3cfce3eee93776f767ad3da9d222d0
Jem <jemalvarez22@gmail.com>
2019-03-14 21:04:40 -0400

    Revert "Add loading spinner"
    This reverts commit 97499f4790b01b71320e4bcccb06b8460f2d7429.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import ContactPage from '../components/ContactPage'
@@ -7,34 +6,21 @@ import Footer from '../components/Footer'
 import MainPage from '../components/MainPage'
 import Navbar from '../components/Navbar'
 import ProjectsPage from '../components/ProjectsPage'
-import Spinner from '../components/Spinner'
 
 export const history = createHistory();
 
-class AppRouter extends React.Component {
-  componentWillMount() {
-    window.addEventListener('load', () => {
-      document.querySelector('body').setAttribute('style', 'overflow: auto')
-      document.querySelector('.sweet-loading').remove()
-    })
-  }
-
-  render() {
-    return (
-      <Router history={history}>
-        <div id="app-wrapper">
-          <Spinner />
-          <Navbar />
-          <Switch>
-            <Route path="/" component={MainPage} exact={true} />
-            <Route path="/contact" component={ContactPage} />
-            <Route path="/projects" component={ProjectsPage} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    )
-  }
-}
+const AppRouter = () => (
+  <Router history={history}>
+    <div id="app-wrapper">
+      <Navbar />
+      <Switch>
+        <Route path="/" component={MainPage} exact={true} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/projects" component={ProjectsPage} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default AppRouter;
