77ba4b6e731cf6a3bf1d8eb7b20c3293c58871c1
Jem <jemalvarez22@gmail.com>
2019-03-16 19:55:36 -0400

    Google map styles and correct location + modal overflow ios fix?

--- src/components/MapModal.js
+++ src/components/MapModal.js
@@ -8,8 +8,8 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>
 class Map extends React.Component {
     static defaultProps = {
         center: {
-            lat: 59.95,
-            lng: 30.33
+            lat: 25.47,
+            lng: -80.48
         },
         zoom: 11
     };
@@ -27,10 +27,11 @@ class Map extends React.Component {
                         bootstrapURLKeys={{ key: 'AIzaSyDH-cRglkSxWC0YPIyNNsU6uwxEn7vgLWI' }}
                         defaultCenter={this.props.center}
                         defaultZoom={this.props.zoom}
+                        options={mapOptions}
                     >
                         <AnyReactComponent
-                            lat={59.955413}
-                            lng={30.337844}
+                            lat={this.props.center.lat}
+                            lng={this.props.center.lng}
                             text={'Kreyser Avrora'}
                         />
                     </GoogleMapReact>
@@ -44,6 +45,146 @@ class Map extends React.Component {
             </Modal>
         );
     }
+}
+
+const mapOptions = {
+    styles: [
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 13
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#144b53"
+                },
+                {
+                    "lightness": 14
+                },
+                {
+                    "weight": 1.4
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#08304b"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#0c4152"
+                },
+                {
+                    "lightness": 5
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#0b434f"
+                },
+                {
+                    "lightness": 25
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#0b3d51"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#146474"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#021019"
+                }
+            ]
+        }
+    ]
 }
 
 export default Map
